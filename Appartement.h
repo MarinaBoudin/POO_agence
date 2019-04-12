8ecaab0eed2de98e8a2ae5bc0fa5518ea0700b5f
MarinaBoudin <marina.boudin@etu.u-bordeaux.fr>
2019-04-12 13:45:43 +0200

    Appartement

--- Appartement.h
+++ Appartement.h
@@ -1,8 +1,10 @@
 #ifndef PROJECT_APPARTEMENT_H
 #define PROJECT_APPARTEMENT_H
 
+#include "Adresse.h"
+#include "Bien.h"
 
-class Appartement {
+class Appartement : public Bien{
   private:
     int pieces;
     int etage;
@@ -10,6 +12,14 @@ class Appartement {
     bool cave;
     bool balcon;
     int nb_appart;
+  public:
+    Appartement(int _prix, Adresse _adresse, int _m2, int _ref_client, int _ref_catalogue,int _pieces,int _etage,bool _garage,bool _cave,bool _balcon,int _nb_appart);
+    int get_pieces();
+    int get_etages();
+    bool get_garage();
+    bool get_cave();
+    bool get_balcon();
+    int get_nb_appart();
 };
 
 
