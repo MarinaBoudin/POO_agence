6e6719819c9f0f5a05f0dc0c1913249e47e5b32e
MarinaBoudin <marina.boudin@etu.u-bordeaux.fr>
2019-04-12 14:05:47 +0200

    Local

--- Local.h
+++ Local.h
@@ -1,11 +1,16 @@
 #ifndef PROJECT_LOCAL_H
 #define PROJECT_LOCAL_H
 
+#include "Bien.h"
 
-class Local {
+class Local : public Bien{
   private:
     int taille_vitrine;
     bool espace_stockage;
+  public:
+    Local(int _prix, Adresse _adresse, int _m2, int _ref_client, int _ref_catalogue,int _taille_vitrine, bool _espace_stockage);
+    int get_taille_vitrine();
+    bool get_espace_stockage();
 };
 
 
