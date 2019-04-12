d8fbf75562a2c19f3dc568c77cdf73b5f2f921d8
MarinaBoudin <marina.boudin@etu.u-bordeaux.fr>
2019-04-12 14:00:26 +0200

    Terrain

--- Terrain.cpp
+++ Terrain.cpp
@@ -1,1 +1,9 @@
 #include "Terrain.h"
+#include "Bien.h"
+
+Terrain::Terrain(int _prix, Adresse _adresse, int _m2, int _ref_client, int _ref_catalogue,bool _constructive):Bien(_prix, _adresse, _m2, _ref_client, _ref_catalogue){
+  constructive=_constructive;
+}
+bool Terrain::get_constructive(){
+  return constructive;
+}
--- Terrain.h
+++ Terrain.h
@@ -1,10 +1,14 @@
 #ifndef PROJECT_TERRAIN_H
 #define PROJECT_TERRAIN_H
 
+#include "Bien.h"
 
-class Terrain {
+class Terrain : public Bien{
   private:
     bool constructive;
+  public:
+    Terrain(int _prix, Adresse _adresse, int _m2, int _ref_client, int _ref_catalogue,bool _constructive);
+    bool get_constructive();
 };
 
 
