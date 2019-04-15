49f8e6d0c67cc44460bddfc42e00654581e02abe
CharlotteAndre <charlotte-andre@hotmail.fr>
2019-04-15 11:14:41 +0200

    Update acheteur

--- Acheteur.cpp
+++ Acheteur.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Acheteur.h"
+#include <map>
 
 using namespace std;
 
@@ -11,5 +12,20 @@ string Acheteur::getnom() { return nom; }
 
 void Acheteur::show() {
     cout << "L'acheteur " << nom << " habite au " << endl;
+    for (const auto &[k, v] : visites){
+        std::cout << "bien:[" << k << "] = (" << v.first << ", " << v.second << ") " << std::endl;
+    }
     adresse.show();
+}
+
+
+void Acheteur::Avisiter(string nomBien,int prop= 0, int montant=0){
+    visites[nomBien]["Proposition"]=prop;
+    visites[nomBien]["Montant"]=montant;
+
+
+}
+
+bool Acheteur::operator<(const Acheteur &other) {
+    return false;
 }
